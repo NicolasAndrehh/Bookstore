30006187d9cc12d1273472e334528e1f534bbef3
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-03-29 11:41:05 -0500

    Add Routes for Booklist and Categories

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
+import Booklist from './components/booklist';
+import Header from './components/header';
+import Categories from './components/categories';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+        <Header />
+
+        <Routes>
+          <Route index element={<Booklist />} />
+          <Route path="/" element={<Booklist />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
