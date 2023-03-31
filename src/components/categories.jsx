436a75b15e18e74e9fa1c918f831e41a6a02ec6a
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-03-30 19:28:43 -0500

    Add categories reducers into component

--- src/components/categories.jsx
+++ src/components/categories.jsx
@@ -1,3 +1,16 @@
-const Categories = () => <button type="button">Check Status</button>;
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const [showStatus, setShowStatus] = useState(false);
+
+  return (
+    <>
+      <button type="button" onClick={() => setShowStatus(true)}>Check Status</button>
+      {showStatus ? <p>{categories.status}</p> : null}
+    </>
+  );
+};
 
 export default Categories;
