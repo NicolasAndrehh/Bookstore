6c6b9f17da8b0893777ebc3574aa1cf5921fe2c3
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-04-03 18:41:49 -0500

    Add header styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,13 @@
 import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-  <header>
-    <nav className="navbar">
-      <div style={{ display: 'flex' }}>
-        <h1>Bookstore</h1>
-        <NavLink to="/">Books</NavLink>
-        <NavLink to="/categories">Categories</NavLink>
-      </div>
-    </nav>
-  </header>
+  <nav className="navbar">
+    <div className="nav-links">
+      <h1>Bookstore CMS</h1>
+      <NavLink to="/">Books</NavLink>
+      <NavLink to="/categories">Categories</NavLink>
+    </div>
+  </nav>
 );
 
 export default Header;
